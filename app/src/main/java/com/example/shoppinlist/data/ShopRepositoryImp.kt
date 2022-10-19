e579abe17ee76c7b7c1a2f310b49cdefa37d505b
dinmuxammedmamanov <dimamanov37@gmail.com>
2022-10-19 12:05:18 +0500

    recyclerview

--- app/src/main/java/com/example/shoppinlist/data/ShopRepositoryImp.kt
+++ app/src/main/java/com/example/shoppinlist/data/ShopRepositoryImp.kt
@@ -4,15 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.shoppinlist.domain.ShopItem
 import com.example.shoppinlist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopRepositoryImp : ShopListRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({o1,o2 -> o1.id.compareTo(o2.id)})
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 .. 10) {
-            val item = ShopItem("Name $i",i,true)
+        for (i in 0 .. 1000) {
+            val item = ShopItem("Name $i",i, Random.nextBoolean())
             addShopItem(item)
         }
     }
